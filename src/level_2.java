28536d359073537ed3e0f3307765622a9f6244cc
Arif Shahriar <61360727+Arif-Shahriar028@users.noreply.github.com>
2020-04-03 11:22:28 +0600

    Add files via upload

---
+++ src/level_2.java
@@ -0,0 +1,215 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Timer;
+import javax.swing.JLabel;
+import java.util.TimerTask;
+
+ 
+public class level_2 extends JFrame{
+	JLabel display;
+	static int a = 0;	
+    
+    int w = 50; int h = 50;
+	int x = 51; int y =50;
+    private int[][] maze = {
+		{1,1,1,1,1,1,1,1,1,1,1,1,1},
+		{1,0,0,0,1,0,0,0,0,0,0,0,1},
+		{1,1,1,0,1,0,1,0,1,1,0,1,1},
+		{1,0,0,0,0,1,0,0,1,0,0,0,1},
+		{1,1,0,1,1,0,0,1,1,0,1,0,1},
+		{1,0,0,1,0,0,1,0,0,0,1,1,1},
+		{1,0,1,0,1,0,1,1,1,0,0,0,1},
+		{1,0,0,0,1,0,0,0,1,1,1,0,1},
+		{1,0,1,0,0,0,1,1,0,0,0,0,1},
+		{1,0,1,0,1,0,0,1,9,1,0,1,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+
+	//constructor
+	level_2(){
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.setLayout(null);
+		Timer t = new Timer();
+		display = new JLabel();
+		JLabel msg = new JLabel("Time Left: ");
+		this.add(display);
+		this.add(msg);
+		display.setBounds(800, 100, 100, 50);
+		msg.setBounds(700, 100, 100, 50);
+		time(t);
+		this.addWindowListener(new WindowAdapter(){
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Exit(t);
+            }
+            
+        	});
+	}
+	
+	//Exit the running game giving a warning message
+	void Exit(Timer t){
+		int x = JOptionPane.showConfirmDialog(null, "Do you want to quit game?", "Cancel", JOptionPane.YES_NO_OPTION);
+                if(x == JOptionPane.YES_OPTION){
+		    dispose();
+		    t.cancel();
+		    Newframe f = new Newframe();
+		    f.setVisible(true);
+		    f.setBounds(200, 200, 600, 300);
+                    
+                }
+	}
+	
+	//Timer 
+	void time(Timer t){
+		 	
+		t.scheduleAtFixedRate(new TimerTask(){
+                    int i = 20;
+                    @Override
+                    public void run() {
+                        
+                        display.setText(""+(i--));
+						if(a==1)
+						{
+							w =20 ; h = 20;
+							a = 0;
+							t.cancel();
+						}
+							
+                                                else if(i < 0)
+						{
+						        dispose();
+							t.cancel();
+							w = 20; h = 20;
+							JOptionPane.showMessageDialog(null, "you loose!");
+							Newframe f = new Newframe();
+			                                f.setVisible(true);	
+							
+						}
+                            
+                    }
+                }, 0, 1200);
+	}
+	
+	//Abstract method graphics to draw the rectangle
+    public void paint(Graphics g) {
+        //super.paint(g);
+        
+        g.translate(40, 40);  //Translates the origin of the graphics context to the point (x, y) in the current coordinate system.
+        
+        // draw the maze
+        
+        for(int row = 0; row < maze.length; row++){
+            //q = 0;
+            for(int col = 0; col < maze[row].length; col++){
+                Color color;
+                switch(maze[row][col]){
+                    case 1 : color = Color.BLACK; break;
+                    case 9 : color = Color.GREEN; break;
+                    default : color = Color.WHITE; break;
+                    
+                }
+              g.setColor(color);
+              g.fillRect(50*col, 50*row, 50, 50);
+            }
+        }
+        if(x==50 && y==30)
+           g.setColor(Color.MAGENTA);
+	    else if(x==30&& y==50)
+			g.setColor(Color.PINK);
+		else if(x==51&&y==50)
+			g.setColor(Color.BLUE);
+		else
+			g.setColor(Color.RED);
+		
+        g.fillOval(w, h, x, y); //Fills an oval bounded by the specified rectangle with the current color.
+    }
+	
+  	//key event to move the user object.
+    @Override
+    protected void processKeyEvent(KeyEvent ke) {
+		int i = 0;
+        if (ke.getID() != KeyEvent.KEY_PRESSED) {
+            return;
+        }
+        if (ke.getKeyCode() == KeyEvent.VK_DOWN) {
+			if(maze[(h+50)/50][w/50]!=1)
+			{ 
+                if(maze[(h+50)/50][w/50]==9)
+				    i =1;		
+			h = h+50 ;
+			if(h >450)
+				h =450;
+			x = 30; y = 50;
+                          
+			}		
+        }
+        else if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+		   if(maze[(h)/50][(w+50)/50]!=1)
+		   {
+                       if(maze[(h)/50][(w+50)/50]==9)
+		            i = 1;
+                                
+	             w = w+50 ;
+		     if(w>550)
+			   w= 550;
+		     x = 50; y= 30;
+		   }
+           
+        }
+		else if(ke.getKeyCode() == KeyEvent.VK_LEFT){
+			if(maze[(h)/50][(w-50)/50]!=1)
+			{
+                            if(maze[(h)/50][(w-50)/50]==9)
+				i = 1;
+			 w = w-50;
+			 if(w <50)
+				w = 50;
+			}
+			x = 50; y = 30;
+			
+		}
+		else if(ke.getKeyCode() == KeyEvent.VK_UP){
+			if(maze[(h-50)/50][(w)/50]!=1)
+			{
+                             if(maze[(h-50)/50][w/50]==9)
+				 i = 1;
+                                
+			h = h-50;
+			if(h <50)
+				h = 50;
+			}
+			
+			x = 30; y=50;			
+		}
+        repaint(); 
+	    	//when user can reach the final destination
+		if(i == 1 && a==0)
+		{
+			a = 1;
+			x = 50; y = 50;
+			repaint();
+			ImageIcon icon = new ImageIcon("con.png");
+            JOptionPane.showMessageDialog(null, "", 
+            "Congratulations", JOptionPane.INFORMATION_MESSAGE, icon);
+			Newframe f = new Newframe();
+			f.setVisible(true);
+			f.setBounds(200, 200, 600, 300);
+			dispose();
+		}
+			
+    }
+	//Main method
+    public static void main(String[] args) {
+        level_2 frame = new level_2();
+        frame.setVisible(true);
+        frame.setBounds(200, 200, 800, 800);
+        frame.setTitle("Graphics Design");  
+        frame.setLocationRelativeTo(null);      
+    }
+}
---
+++ src/level_3.java
@@ -0,0 +1,226 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.util.Timer;
+import javax.swing.JLabel;
+import java.util.TimerTask;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+ 
+public class level_3 extends JFrame{
+	int x;
+	
+	JLabel display, msg;
+	
+	static int a = 0;
+    int w = 20; int h = 20;
+	
+	private int[][] maze = {
+	    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,1,1,1},
+        {1,1,0,1,1,1,1,1,0,1,0,1,0,1,0,1,0,0,1,0,0,0,1,1,1,0,0,1,0,0,0,1},
+        {1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,0,1},
+        {1,1,0,1,0,1,1,0,0,1,0,1,1,1,1,1,1,0,1,1,0,1,1,0,0,0,0,0,0,1,0,1},
+        {1,1,0,1,1,0,1,0,1,0,0,0,0,0,1,0,0,0,1,1,0,1,1,0,1,0,1,1,0,1,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,1,1,1,0,1,0,0,1,0,0,0,0,1,0,1},
+        {1,0,1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,1,1,0,1,1,0,1,1,1,1,0,1,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,1,0,0,0,0,1,1,1,0,0,0,0,0,0,1},
+        {1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,0,1,0,1,0,1,1,0,0,0,1,1,0,1,0,1},
+        {1,0,1,0,0,0,0,0,0,0,1,0,1,0,1,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1,0,1},
+        {1,0,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,1,0,1,0,0,1,1,0,0,1,1,0,1,0,1},
+        {1,0,0,0,1,0,0,0,0,0,0,1,0,1,0,1,1,1,0,1,1,0,0,0,0,0,1,1,0,1,0,1},
+        {1,0,1,0,1,0,1,1,1,1,1,1,0,0,0,1,0,0,0,1,1,0,1,0,1,1,1,1,1,0,0,1},
+        {1,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1,1,0,0,0,1,0,0,0,0,0,0,1,1,1},
+        {1,0,1,0,1,1,1,1,1,1,1,1,0,1,1,0,0,1,1,0,1,0,1,0,1,1,1,1,0,1,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,1,1,0,0,0,0,1,0,0,0,1},
+        {1,0,1,1,1,0,1,0,1,0,1,1,1,0,0,1,1,0,1,1,0,0,0,1,1,1,0,1,0,1,1,1},
+        {1,0,0,0,1,0,1,0,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,0,0,0,0,1,0,0,0,1},
+        {1,1,1,0,1,0,1,0,1,0,1,0,1,1,0,1,0,0,1,0,1,0,1,1,1,1,0,1,0,1,0,1},
+        {1,0,1,1,1,0,1,0,1,0,1,0,1,1,0,1,0,1,1,1,1,0,1,0,0,1,0,0,0,1,0,1},
+        {1,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,1},
+        {1,1,1,0,1,0,1,0,1,0,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,1,0,1,1},
+        {1,0,1,0,1,0,0,0,1,0,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,1,0,1,1},
+        {1,0,1,0,1,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,0,1,1},
+        {1,0,1,0,1,1,1,0,1,0,1,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,0,1,1},
+        {1,0,0,0,0,0,1,0,1,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,1,1},
+        {1,1,0,1,1,0,0,0,1,0,1,1,0,0,0,0,1,1,0,1,0,1,0,0,0,0,0,0,0,0,1,1},
+        {1,1,0,1,0,1,0,1,1,0,1,0,1,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,0,1,1},
+	    {1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,9,1},
+	    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+	
+	
+	//Constructor
+	level_3(){
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.setLayout(null);
+		Timer t = new Timer();
+		display = new JLabel();
+		msg = new JLabel("Time Left: ");
+		this.add(display);
+		this.add(msg);
+		display.setBounds(800, 100, 100, 50);
+		msg.setBounds(700, 100, 100, 50);
+		time(t);
+		this.addWindowListener(new WindowAdapter(){
+            	@Override
+            	public void windowClosing(WindowEvent e) {
+                	Exit(t);
+            	}
+            
+        	});
+	}
+	// Exit the running game by giving a warning message
+	void Exit(Timer t){
+		 x = JOptionPane.showConfirmDialog(null, "Do you want to quit game?", "Cancel", JOptionPane.YES_NO_OPTION);
+                if(x == JOptionPane.YES_OPTION){
+		    dispose();
+		    t.cancel();
+		    Newframe f = new Newframe();
+		    f.setVisible(true);
+		    f.setBounds(200, 200, 600, 300);
+                    
+                }
+	}
+	
+	// Timer
+	void time(Timer t){
+		 	
+		t.scheduleAtFixedRate(new TimerTask(){
+                    int i = 20;
+                    @Override
+                    public void run() {
+                        
+                        display.setText(""+(i--));
+						if(a==1)
+						{
+							w =20 ; h = 20;
+							a = 0;
+							t.cancel();
+						}
+							
+                                                else if(i < 0)
+						{
+						        dispose();
+							t.cancel();
+							w = 20; h = 20;
+							JOptionPane.showMessageDialog(null, "you loose!");
+							Newframe f = new Newframe();
+			                                f.setVisible(true);	
+							
+						}
+                            
+                    }
+                }, 0, 1200);
+	}
+ 
+	
+	// Abstract method graphics to draw the rectangle on JFrame.
+    public void paint(Graphics g) {
+        //super.paint(g);
+        
+        g.translate(05, 35);  //Translates the origin of the graphics context to the point (x, y) in the current coordinate system.
+        
+        
+        for(int row = 0; row < maze.length; row++){
+            for(int col = 0; col < maze[row].length; col++){
+                Color color;
+                switch(maze[row][col]){
+                    case 1 : color = Color.BLACK; break;
+                    case 9 : color = Color.GREEN; break;
+                    default : color = Color.WHITE; break;
+                    
+              }
+              g.setColor(color);
+	      g.fillRect(20*col, 20*row, 20, 20);
+            }
+        }
+        
+        g.setColor(Color.MAGENTA);
+        g.fillOval(w, h, 20, 20); //Fills an oval bounded by the specified rectangle with the current color.
+    }
+	
+    // Key Event to move the object of user
+    @Override
+    protected void processKeyEvent(KeyEvent ke) {
+	int i = 0;
+        if (ke.getID() != KeyEvent.KEY_PRESSED) {
+            return;
+        }
+	    
+        if (ke.getKeyCode() == KeyEvent.VK_DOWN) {
+	    if(maze[(h+20)/20][w/20]!=1)
+	    { 
+                if(maze[(h+20)/20][w/20]==9)
+	            i =1;		
+	        h = h+20 ;
+		if(h >600)
+		    h =600;   
+	    }	
+        }
+	    
+        else if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+	      if(maze[(h)/20][(w+20)/20]!=1)
+	      {
+                if(maze[(h)/20][(w+20)/20]==9)
+		     i = 1;
+                                
+		w = w+20 ;
+	        if(w>600)
+		    w= 600;
+	      }
+           
+        }
+	else if(ke.getKeyCode() == KeyEvent.VK_LEFT){
+		if(maze[(h)/20][(w-20)/20]!=1)
+		{
+                    if(maze[(h)/20][(w-20)/20]==9)
+          		i = 1;
+		    w = w-20;
+		    if(w <20)
+		        w = 20;
+		}
+			
+	}
+	else if(ke.getKeyCode() == KeyEvent.VK_UP){
+		if(maze[(h-20)/20][(w)/20]!=1)
+		{
+                    if(maze[(h-20)/20][w/20]==9)
+			i = 1;
+                                
+		    h = h-20;
+		    if(h <20)
+			 h = 20;
+		}
+	}
+        repaint(); 
+		
+		if(i == 1 && a==0)
+		{
+			//repaint();
+			a = 1;
+			ImageIcon icon = new ImageIcon("con.png");
+            JOptionPane.showMessageDialog(null, "","Congratulations", JOptionPane.INFORMATION_MESSAGE, icon);
+			Newframe f = new Newframe();
+			f.setVisible(true);
+			//f.setBounds(200, 200, 800, 800);
+			dispose();
+		}
+			
+    }              
+
+    // Main method
+    public static void main(String[] args) {
+        level_3 frame = new level_3();
+		//level_3 ob = new level_3(1);
+        frame.setVisible(true);
+        frame.setBounds(0, 0, 850, 665);
+	frame.setTitle("Escape Maze - Level 3");
+        frame.setLocationRelativeTo(null);    
+    }
+}	 
